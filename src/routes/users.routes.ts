9144616023a25c4fb029cfc0e7f98f13046d3ced
Paulo-Tonelotti <paulohenrique63664@gmail.com>
2023-05-17 18:39:39 -0300

    remove auth route in user

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,6 +1,5 @@
 import { Router } from 'express';
 import { CreateUserController } from '../modules/user/useCases/createUser/CreateUserController';
-import { ensureAuthenticate } from '../middlewares/ensureAuthenticated';
 import { AddTeamUserController } from '../modules/user/useCases/addTeamUser/AddTeamUserController';
 import { FindTransferController } from '../modules/user/useCases/findTransfersTeamUser/FindTransferController';
 import { AddLeagueController } from '../modules/user/useCases/addLeague/AddLeagueController';
@@ -13,21 +12,9 @@ const findTransferController = new FindTransferController();
 const addLeagueController = new AddLeagueController();
 
 usersRoutes.post('/users', createUserController.handle);
-usersRoutes.post(
-  '/users/addTeam',
-  ensureAuthenticate,
-  addTeamUserController.handle
-);
-usersRoutes.get(
-  '/users/findTransfer',
-  ensureAuthenticate,
-  findTransferController.handle
-);
+usersRoutes.post('/users/addTeam', addTeamUserController.handle);
+usersRoutes.get('/users/findTransfer', findTransferController.handle);
 
-usersRoutes.post(
-  '/users/addLeague',
-  ensureAuthenticate,
-  addLeagueController.handle
-);
+usersRoutes.post('/users/addLeague', addLeagueController.handle);
 
 export { usersRoutes };
